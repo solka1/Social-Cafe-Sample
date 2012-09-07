43ca53829d9d58e1b93d16a97eddc8aa2084ada1
Niall Kennedy <niall@niallkennedy.com>
2012-09-06 22:08:13 -0700

    HTML document. properly declare CURIEs for namespace in products.php. expand OGP properties including structured image to set a good example

--- types/coffee.php
+++ types/coffee.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* Copyright 2011 Facebook, Inc.
+* Copyright 2012 Facebook, Inc.
 *
 * Licensed under the Apache License, Version 2.0 (the "License"); you may
 * not use this file except in compliance with the License. You may obtain
@@ -14,56 +14,57 @@
 * License for the specific language governing permissions and limitations
 * under the License.
 */
-  require_once('../products.php');
+require_once(dirname(dirname(__FILE__)).'/products.php');
 
-?><html>
-    <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# social-cafe: http://ogp.me/ns/apps/social-cafe#">
-        <title>OG Sample Object - Coffee</title>
-          <meta property="og:title" content="Coffee" />
-          <meta property="og:image" content="https://s-static.ak.fbcdn.net/images/devsite/attachment_blank.png" />
-          <meta property="og:determiner" content="a" />
-          <meta property="fb:app_id" content="<?php echo $app_id; ?>" />
-          <meta property="og:url" content="<?php echo $app_url; ?>/types/coffee.php" />
-          <meta property="og:type" content="<?php echo $app_namespace; ?>:beveragecategory" />
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head prefix="og: http://ogp.me/ns# <?php echo $app_namespace; ?>: http://ogp.me/ns/apps/<?php echo $app_namespace; ?>#">
+  <meta charset="utf-8">
+  <title>OG Sample Object - Coffee</title>
+  <meta property="og:locale" content="en_US">
+  <meta property="og:title" content="Coffee">
+  <meta property="og:image" content="https://s-static.ak.fbcdn.net/images/devsite/attachment_blank.png">
+  <meta property="og:image:type" content="image/png">
+  <meta property="og:image:width" content="52">
+  <meta property="og:image:height" content="52">
+  <meta property="og:determiner" content="a">
+  <meta property="http://ogp.me/ns/fb#app_id" content="<?php echo $app_id; ?>">
+  <meta property="og:url" content="<?php echo $app_url; ?>/types/coffee.php">
+  <meta property="og:type" content="<?php echo $app_namespace; ?>:beveragecategory">
 
-
-                          <link type="text/css" rel="stylesheet" href="/stylesheets/app.css" />
-    </head>
-    <body>
-        <div id="wrapper">
-            <!-- Information below is for debugging purposes -->
-            <h1>OG Sample Object - Coffee</h1>
-            <table border="0" cellspacing="0">
-                                            <tr>
-              <th class="key">og:title</th>
-
-              <td class="value">Coffee</td>
-              </tr>
-                                                            <tr>
-              <th class="key">og:image</th>
-              <td class="value">https://s-static.ak.fbcdn.net/images/devsite/attachment_blank.png</td>
-              </tr>
-                                                            <tr>
-
-              <th class="key">og:determiner</th>
-              <td class="value">a</td>
-              </tr>
-                                                            <tr>
-              <th class="key">fb:app_id</th>
-              <td class="value"><?php echo $app_id; ?></td>
-              </tr>
-
-                                                            <tr>
-              <th class="key">og:url</th>
-              <td class="value"><?php echo $app_url; ?>/types/coffee.php</td>
-              </tr>
-                                                            <tr>
-              <th class="key">og:type</th>
-              <td class="value"><?php echo $app_namespace; ?>:beveragecategory</td>
-
-              </tr>
-                                          </table>
-        </div>
-    </body>
+  <link rel="stylesheet" type="text/css" href="../css/sc.css">
+</head>
+<body>
+  <div id="wrapper">
+    <!-- Information below is for debugging purposes -->
+    <h1>OG Sample Object - Coffee</h1>
+    <table style="border:0;border-spacing:0;border-collapse:collapse">
+      <tr>
+        <th class="key">og:title</th>
+        <td class="value">Coffee</td>
+      </tr>
+      <tr>
+        <th class="key">og:image</th>
+        <td class="value">https://s-static.ak.fbcdn.net/images/devsite/attachment_blank.png</td>
+      </tr>
+      <tr>
+        <th class="key">og:determiner</th>
+        <td class="value">a</td>
+      </tr>
+      <tr>
+        <th class="key">fb:app_id</th>
+        <td class="value"><?php echo $app_id; ?></td>
+      </tr>
+      <tr>
+        <th class="key">og:url</th>
+        <td class="value"><?php echo $app_url; ?>/types/coffee.php</td>
+      </tr>
+      <tr>
+        <th class="key">og:type</th>
+        <td class="value"><?php echo $app_namespace; ?>:beveragecategory</td>
+      </tr>
+    </table>
+  </div>
+</body>
 </html>
-
